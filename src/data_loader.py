9dcdd2c77395aa8cc53cf6b7ab83ed7fc3a11193
Sumezi <mchr270902@gmail.com>
2025-07-09 17:53:07 +0700

    Add val dataloader

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,16 +7,10 @@ from torcheeg.datasets import BCICIV2aDataset
 
 
 class MotorImageryDataLoader:
-    def __init__(self,
-                data_dir="./data",
-                chunk_size=1750,
-                num_channel=22,
-                test_size=0.2,
-                random_state=None,
-                shuffle=False,
-                io_path=None,
-                split_path=None,
-                num_worker=4):
+    def __init__(self, data_dir="./data", chunk_size=1750, num_channel=22, test_size=0.2, val_size=0.1, 
+                random_state=None, io_path=None, split_path=None, num_worker=4):
+
+        self.val_size = val_size
 
         # Download if needed
         self.mat_folder = Path(data_dir) / "BCICIV_2a_mat"
@@ -43,7 +37,6 @@ class MotorImageryDataLoader:
         self.train_dataset, self.test_dataset = model_selection.train_test_split(
             dataset=self.dataset,
             test_size=test_size,
-            shuffle=shuffle,
             random_state=random_state,
             split_path=split_path
         )
@@ -51,14 +44,16 @@ class MotorImageryDataLoader:
     def _download_if_needed(self):
         base_url = "http://bnci-horizon-2020.eu/database/data-sets/001-2014/"
         files = ["A01T.mat", "A01E.mat", "A02T.mat", "A02E.mat", "A03T.mat", "A03E.mat",
-                "A04T.mat", "A04E.mat", "A05T.mat", "A05E.mat", "A06T.mat", "A06E.mat",
-                "A07T.mat", "A07E.mat", "A08T.mat", "A08E.mat", "A09T.mat", "A09E.mat"]
+                 "A04T.mat", "A04E.mat", "A05T.mat", "A05E.mat", "A06T.mat", "A06E.mat",
+                 "A07T.mat", "A07E.mat", "A08T.mat", "A08E.mat", "A09T.mat", "A09E.mat"]
 
         self.mat_folder.mkdir(parents=True, exist_ok=True)
 
-        missing_files = [f for f in files if not (self.mat_folder / f).exists()]
+        missing_files = [f for f in files if not (
+            self.mat_folder / f).exists()]
         if missing_files:
-            print(f"BCICIV_2a dataset is missing. Downloading {len(missing_files)} files...")
+            print(
+                f"BCICIV_2a dataset is missing. Downloading {len(missing_files)} files...")
 
         for filename in files:
             file_path = self.mat_folder / filename
@@ -68,13 +63,22 @@ class MotorImageryDataLoader:
 
     def get_train_loader(self, batch_size=16, shuffle=True, num_workers=0):
         return DataLoader(self.train_dataset, batch_size=batch_size,
-                            shuffle=shuffle, num_workers=num_workers, pin_memory=True)
+                          shuffle=shuffle, num_workers=num_workers, pin_memory=True)
 
     def get_test_loader(self, batch_size=16, shuffle=False, num_workers=0):
         return DataLoader(self.test_dataset, batch_size=batch_size,
-                            shuffle=shuffle, num_workers=num_workers, pin_memory=True)
+                          shuffle=shuffle, num_workers=num_workers, pin_memory=True)
 
-    def get_loaders(self, batch_size=16, shuffle_train=True, num_workers=0):
-        # Dataloader shape: (batch_size, 1, num_channel, chunk_size)
-        return (self.get_train_loader(batch_size, shuffle_train, num_workers),
-                self.get_test_loader(batch_size, False, num_workers))
+    def get_loaders(self, batch_size=16, shuffle=True, num_workers=0):
+        train_dataset, val_dataset = model_selection.train_test_split(
+            dataset=self.train_dataset,
+            test_size=self.val_size,
+            shuffle=shuffle,
+            random_state=42
+        )
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle,
+                                num_workers=num_workers, pin_memory=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, 
+                                num_workers=num_workers, pin_memory=True)
+        test_loader = self.get_test_loader(batch_size, False, num_workers)
+        return train_loader, val_loader, test_loader
